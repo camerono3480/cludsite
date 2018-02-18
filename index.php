64c712bffc25905e250c09b68713e928ffd5efcd
camerono3480 <cambear50@gmail.com>
2018-02-18 16:57:10 +1300

    update

--- index.php
+++ index.php
@@ -13,10 +13,17 @@
    </head>
    <body>
      <form action="signup.php" method="post">
-       <input type="users" name="username" value="username" required="TRUE">
-       <input type="password" name="password" value="password" required="TRUE">
-       <input type="email" name="email" value="email" required="TRUE">
-       <input type="submit" name="submit" value="submit">
+       <input type="users" name="username" value="username" required="TRUE"><br>
+       <input type="password" name="password" value="password" required="TRUE"><br>
+       <input type="email" name="email" value="email" required="TRUE"><br>
+       <input type="submit" name="submit" value="submit"><br>
+
+     </form><br><br><br><br>
+     <form action="login.php" method="post">
+       <input type="username" name="username" value="" required="TRUE"><br>
+       <input type="password" name="password" value="" required="TRUE"><br>
+       <input type="submit" name="submit" value="submit"><br>
+
      </form>
   <div class="heada">
 
---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+require 'init__/__init__.php';
+if (isset($_POST['username'])&&isset($_POST['password'])) {
+   $password = $_POST['password'];
+   $username = $_POST['username'];
+
+   $sql = "SELECT salt from `users` where username='$username'";
+
+   $result = mysqli_query($conn, $sql);
+
+    if($result->num_rows > 0){
+      while ($row = mysqli_fetch_assoc($result)) {
+
+
+        $_PASS = hash('sha256', $row['salt'].$password);
+
+        $sqli = "SELECT password from `users` where username='$username'";
+        $sli = mysqli_query($conn,$sqli);
+
+        $sil = mysqli_fetch_assoc($sli);
+        if ($_PASS == $sil['password']) {
+          echo "success";
+           $_SESSION['user']=$username;
+           $_SESSION['logged'] =  $username&&$_PASS;
+
+        }else {
+          echo "error";
+        }
+      }
+
+
+    }
+
+}
+ ?>
